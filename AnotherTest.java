c21e2f470992637091131a6aa9dac9a0f0f9b5d1
Paul Sho <Cyberkiller.CK@gmail.com>
2022-10-08 21:58:34 +0800

    Create AnotherTest.java

---
+++ AnotherTest.java
@@ -0,0 +1,16 @@
+public class AnotherTest {
+    public static void main(String[] args) {
+
+        // int numbers = 1234567890;
+        // int lengthOfNumbers = String.valueOf(numbers).length();
+        // System.out.println(lengthOfNumbers);
+
+        int numbers2 = 12345;
+        int lastIndex = String.valueOf(numbers2).lastIndexOf('5');
+        System.out.println(lastIndex);
+
+        String word = "Watermelon pan";
+        System.out.println(word.lastIndexOf('p'));
+
+    }
+}
